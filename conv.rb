78227712d7761ff5b29375fcd9f2aac2c4305b36
pintergreg <pintergreg@gmail.com>
2022-07-27 10:12:11 +0200

    Use RIS based input

---
+++ conv.rb
@@ -0,0 +1,13 @@
+require "ref_parsers"
+require "yaml"
+require "fileutils"
+
+Dir.glob("resources/ris/*.ris") do |ris_file|
+    parser = RefParsers::RISParser.new
+    ris = parser.open(ris_file)
+
+    FileUtils.mkdir_p("data/ris")
+    File.open("data/ris/#{File.basename(ris_file, ".ris")}.yaml", "w") do |file|
+        file.write(YAML.dump(ris[0]))
+    end
+end
