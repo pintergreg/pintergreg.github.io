5ab5ae364958e29e67fb0c147f93de1e9f85acc7
pintergreg <pintergreg@gmail.com>
2023-05-26 10:28:34 +0200

    Apply rubocop

--- conv.rb
+++ conv.rb
@@ -1,13 +1,15 @@
-require "ref_parsers"
-require "yaml"
-require "fileutils"
+# frozen_string_literal: true
 
-Dir.glob("resources/ris/*.ris") do |ris_file|
-    parser = RefParsers::RISParser.new
-    ris = parser.open(ris_file)
+require 'ref_parsers'
+require 'yaml'
+require 'fileutils'
 
-    FileUtils.mkdir_p("data/ris")
-    File.open("data/ris/#{File.basename(ris_file, ".ris")}.yaml", "w") do |file|
-        file.write(YAML.dump(ris[0]))
-    end
+Dir.glob('resources/ris/*.ris') do |ris_file|
+  parser = RefParsers::RISParser.new
+  ris = parser.open(ris_file)
+
+  FileUtils.mkdir_p('data/ris')
+  File.open("data/ris/#{File.basename(ris_file, '.ris')}.yaml", 'w') do |file|
+    file.write(YAML.dump(ris[0]))
+  end
 end
