9b7ab076f5e8f4e5f4fb4e8aed0084ffa0dd57ff
pintergreg <pintergreg@gmail.com>
2023-07-04 21:42:56 +0200

    Apply rubocop

--- conv_bib.rb
+++ conv_bib.rb
@@ -1,13 +1,13 @@
-require "bibtex"
-require "yaml"
-require "fileutils"
+require 'bibtex'
+require 'yaml'
+require 'fileutils'
 
 # @attributes = ["author", "title", "journal", "volume", "year", "number", "article-number", "url", "pubmedid", "issn", "abstract", "doi"]
 
-Dir.glob("resources/bibtex/*.bib") do |bibtex|
-    FileUtils.mkdir_p("data/bibtex")
+Dir.glob('resources/bibtex/*.bib') do |bibtex|
+  FileUtils.mkdir_p('data/bibtex')
 
-    File.open("data/bibtex/#{File.basename(bibtex, ".bib")}.xml", "w") do |file|
-        file.write(BibTeX.open(bibtex).to_xml(:extended => true))
-    end
+  File.open("data/bibtex/#{File.basename(bibtex, '.bib')}.xml", 'w') do |file|
+    file.write(BibTeX.open(bibtex).to_xml(extended: true))
+  end
 end
