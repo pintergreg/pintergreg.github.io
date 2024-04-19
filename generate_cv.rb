b703ea784e655bb77fbeb57d164e1dd046e1ae8f
pintergreg <pintergreg@gmail.com>
2024-04-19 17:44:25 +0200

    Add contact fields from config #8

--- generate_cv.rb
+++ generate_cv.rb
@@ -14,7 +14,14 @@ asciidoc = %{
 
 ### contact
 
-GitHub: https://github.com/<%= config["Params"]["github"] %> |
-GitLab: https://gitlab.com/<%= config["Params"]["gitlab"] %> |
+GitHub: link:https://github.com/<%= config["Params"]["github"] %>[] |
+GitLab: link:https://gitlab.com/<%= config["Params"]["gitlab"] %>[] |
+HackerRank: link:https://www.hackerrank.com/profile/<%= config["Params"]["hackerrank"] %>[] |
+ORCiD: link:https://orcid.org/<%= config["Params"]["orcid"] %>[] |
+Google Scholar: link:https://scholar.google.hu/citations?user=<%= config["Params"]["scholar"] %>[] |
+ResearchGate: link:https://www.researchgate.net/profile/<%= config["Params"]["researchgate"] %>[] |
+LinkedIn: link:https://www.linkedin.com/in/<%= config["Params"]["linkedin"] %>[] |
+Twitter: link:https://twitter.com/<%= config["Params"]["twitter"] %>[] |
+
 }
 puts ERB.new(asciidoc, trim_mode: "-").result(binding)
