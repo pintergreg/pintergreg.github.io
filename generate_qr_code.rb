e5d830a520f41098c7eb4cb3c34da9c32f52daca
pintergreg <pintergreg@gmail.com>
2024-04-19 13:44:54 +0200

    Apply rubocop

--- generate_qr_code.rb
+++ generate_qr_code.rb
@@ -1,10 +1,12 @@
-require "rqrcode"
+# frozen_string_literal: true
 
-qrcode = RQRCode::QRCode.new("https://pintergreg.github.io/business_card")
+require 'rqrcode'
+
+qrcode = RQRCode::QRCode.new('https://pintergreg.github.io/business_card')
 
 svg = qrcode.as_svg(
-  color: "#2d2d2d",
-  shape_rendering: "crispEdges",
+  color: '#2d2d2d',
+  shape_rendering: 'crispEdges',
   module_size: 12,
   standalone: true,
   use_path: true
